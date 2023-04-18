20cd9649793d66157f0041a26ed39322e2afb88a
Valinetsky <highrat@gmail.com>
2023-04-18 14:14:59 +0300

    Seminar01 added

--- Seminar01/VendingMachine/src/App.java
+++ Seminar01/VendingMachine/src/App.java
@@ -10,10 +10,10 @@ public class App {
 
         VendingMachine itemMachin = new VendingMachine(300);
         itemMachin.addProduct(item1);
-        itemMachin.addProduct(new Product("Чипсы", 60.0));
-        itemMachin.addProduct(new Product("Масло", 50.0));
-        itemMachin.addProduct(new Product("Хлеб", 40.0));
-        itemMachin.addProduct(new Product("Снек", 20.0));
+        itemMachin.addProduct(new Product("Chips", 60.0));
+        itemMachin.addProduct(new Product("Oil", 50.0));
+        itemMachin.addProduct(new Product("Breed", 40.0));
+        itemMachin.addProduct(new Product("Snack", 20.0));
         itemMachin.addProduct(new BottleOfWater("Cola", 88.0, 500 ));
         itemMachin.addProduct(new BottleOfWater("Water", 188.0, 1500 ));
 
