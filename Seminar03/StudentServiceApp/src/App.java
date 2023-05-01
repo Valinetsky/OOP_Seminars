06d36a734c23267ae5503d35a78923011746ed4b
Valinetsky <highrat@gmail.com>
2023-05-01 12:00:03 +0300

    Seminar03. Homework ready.

--- Seminar03/StudentServiceApp/src/App.java
+++ Seminar03/StudentServiceApp/src/App.java
@@ -1,51 +1,87 @@
-import java.net.NoRouteToHostException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 import StudentDomen.Student;
 import StudentDomen.StudentGroup;
+import StudentDomen.StudentStream;
 import StudentDomen.User;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        User u1 = new User("Сергей","Иванов",25);
-        Student s1 = new Student("Сергей", "Иванов", 22, (long)101);
-        Student s2 = new Student("Андрей", "Сидоров", 22, (long)111);
-        Student s3 = new Student("Иван", "Петров", 22, (long)121);
-        Student s4 = new Student("Игорь", "Иванов", 23, (long)301);
-        Student s5 = new Student("Даша", "Цветкова", 23, (long)171);
-        Student s6 = new Student("Лена", "Незабудкина", 23, (long)104);
+	public static void main(String[] args) throws Exception {
+		User u1 = new User("Сергей", "Иванов", 25);
+		Student s1 = new Student("Сергей", "Иванов", 22, (long) 101);
+		Student s2 = new Student("Андрей", "Сидоров", 22, (long) 111);
+		Student s3 = new Student("Иван", "Петров", 22, (long) 121);
+		Student s4 = new Student("Игорь", "Иванов", 23, (long) 301);
+		Student s5 = new Student("Даша", "Цветкова", 23, (long) 171);
+		Student s6 = new Student("Лена", "Незабудкина", 23, (long) 104);
+		Student s7 = new Student("Student07", "StuLastName07", 9, (long) 777);
+		Student s8 = new Student("Student08", "StuLastName08", 8, (long) 877);
+		Student s9 = new Student("Student09", "StuLastName09", 7, (long) 977);
 
-        List<Student> listStud = new ArrayList<Student>();
-        listStud.add(s1);
-        listStud.add(s2);
-        listStud.add(s3);
-        listStud.add(s4);
-        listStud.add(s5);
-        listStud.add(s6);
+		List<Student> listStud = new ArrayList<Student>();
 
-        StudentGroup group = new StudentGroup(listStud);
+		listStud.add(s1);
+		listStud.add(s2);
+		listStud.add(s3);
+		listStud.add(s4);
+		listStud.add(s5);
+		listStud.add(s6);
 
-        for(Student stud : group)
-        {
-            System.out.println(stud);
-        }
+		// Создание группы с именем "Group - group"
+		StudentGroup group = new StudentGroup("Group - group", listStud, 100);
 
-        System.out.println("============= после сортировки =============");
-        Collections.sort(group.getStudents());
+		List<Student> listStudNewGroup = new ArrayList<Student>();
+		listStudNewGroup.add(s7);
+		listStudNewGroup.add(s8);
+		listStudNewGroup.add(s9);
 
+		// Создание группы с именем "Group - groupNewGroup"
+		StudentGroup groupNewGroup = new StudentGroup("Group - groupNewGroup", listStudNewGroup, 10);
 
-        for(Student stud : group)
-        {
-            System.out.println(stud);
-        }
-        
+		// for (Student stud : group) {
+		// System.out.println(stud);
+		// }
 
+		// System.out.println("============= после сортировки =============");
+		// Collections.sort(group.getStudents());
 
-        //System.out.println(u1);
-        //System.out.println(s1);
-        //System.out.println(group);
-    }
+		// for (Student stud : group) {
+		// System.out.println(stud);
+		// }
+
+		// System.out.println(u1);
+		// System.out.println(s1);
+		// System.out.println(group);
+		// System.out.println(groupNewGroup);
+
+		System.out.println("+++++++++++++++++++++++++++");
+		System.out.println("+++++++ НОВЫЙ ЗАПУСК ++++++");
+		System.out.println("+++++++++++++++++++++++++++");
+
+		// Создание потока студентов с именем "MSU"
+		StudentStream lomonosov = new StudentStream("MSU", 1);
+		// Наполнение потока
+		lomonosov.addGroupToStream(group);
+		lomonosov.addGroupToStream(groupNewGroup);
+
+		// Вывод потока студентов переопределенным toString
+		System.out.println(lomonosov);
+
+		// Вывод содержимого потока студентов перебором элементов в цикле
+		for (StudentGroup item : lomonosov) {
+			System.out.println(item);
+		}
+
+		// Сортировка элементов потока студентов по размеру групп
+		Collections.sort(lomonosov.getStudentsGroups());
+
+		System.out.println("After sort");
+		// Повторный вывод отсортированных элементов потока студентов
+		for (StudentGroup item : lomonosov) {
+			System.out.println(item);
+		}
+
+	}
 }
--- Seminar03/StudentServiceApp/src/StudentDomen/StudentGroup.java
+++ Seminar03/StudentServiceApp/src/StudentDomen/StudentGroup.java
@@ -3,45 +3,136 @@ package StudentDomen;
 import java.util.Iterator;
 import java.util.List;
 
-public class StudentGroup implements Iterable<Student> {
-    private List<Student> students;
+/**
+ * класс студенческой группы
+ */
+public class StudentGroup implements Iterable<Student>, Comparable<StudentGroup> {
+	/**
+	 * название группы
+	 */
+	private String groupName;
+	/**
+	 * массив студентов
+	 */
+	private List<Student> students;
+	/**
+	 * идентификационный номер группы
+	 */
+	private long groupID;
 
-    public StudentGroup(List<Student> students) {
-        this.students = students;
-    }
+	/**
+	 * @param groupName - название группы
+	 * @param students  - массив студентов
+	 * @param id        - идентификационный номер группы
+	 */
+	public StudentGroup(String groupName, List<Student> students, long id) {
+		this.groupName = groupName;
+		this.students = students;
+		this.groupID = id;
+	}
 
-    public List<Student> getStudents() {
-        return students;
-    }
+	// public StudentGroup(List<Student> students) {
+	// this.students = students;
+	// }
 
-    public void setStudents(List<Student> students) {
-        this.students = students;
-    }
+	/**
+	 * @param students - массив студентов
+	 */
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
 
-    // @Override
-    // public Iterator<Student> iterator() {
-    //     return new StudentGroupIterator(students);
-    // }
+	// ==========================================================
+	/**
+	 * @return - название группы
+	 */
+	public String getGroupName() {
+		return groupName;
+	}
 
-    @Override
-    public Iterator<Student> iterator() {
-        return new Iterator<Student>(){
-            private int index = 0;
-            @Override
-            public boolean hasNext() {
-               return index<students.size();
-            }
-            @Override
-            public Student next() {
-                if(!hasNext())
-                {
-                    return null;
-                }
-                //counter++;
-                return students.get(index++);        
-            }
+	/**
+	 * @return - идентификационный номер группы
+	 */
+	public long getGroupID() {
+		return groupID;
+	}
 
-        };
-    }
-    
+	/**
+	 * @return - количество студентов в группе
+	 */
+	public long getGroupSize() {
+		return this.students.size();
+	}
+
+	/**
+	 * @param groupName - назначение названия группы
+	 */
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	/**
+	 * @return - массив студентов
+	 */
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	/**
+	 * @param student - добавление студента в массив студентов
+	 */
+	public void addStudentToGroup(Student student) {
+		this.students.add(student);
+	}
+	// ==========================================================
+
+	// @Override
+	// public Iterator<Student> iterator() {
+	// return new StudentGroupIterator(students);
+	// }
+
+	@Override
+	public Iterator<Student> iterator() {
+		return new Iterator<Student>() {
+			private int index = 0;
+
+			@Override
+			public boolean hasNext() {
+				return index < students.size();
+			}
+
+			@Override
+			public Student next() {
+				if (!hasNext()) {
+					return null;
+				}
+				// counter++;
+				return students.get(index++);
+			}
+
+		};
+	}
+
+	@Override
+	public String toString() {
+		System.out.printf("Group #%d %s\n", this.groupID, this.groupName);
+		for (Student student : students) {
+			System.out.println(student);
+		}
+		return String.format("> Count of members: %d\n", students.size());
+	}
+
+	/**
+	 * метод сравнения групп по количеству студентов
+	 */
+	@Override
+	public int compareTo(StudentGroup o) {
+		if (this.getGroupSize() == o.getGroupSize()) {
+			return 0;
+		}
+		if (this.getGroupSize() < o.getGroupSize()) {
+			return -1;
+		}
+		return 1;
+	}
 }
